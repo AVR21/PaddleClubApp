069873b5103df980e52f3d845297437e5f3cea8d
Manuel <manuelcntpublica@yahoo.com>
2023-10-20 22:41:39 +0100

    Solved a problem with create(). There was an instance call missing

--- src/org/ulpgc/is1/model/NIF.java
+++ src/org/ulpgc/is1/model/NIF.java
@@ -3,22 +3,22 @@ package org.ulpgc.is1.model;
 public class NIF {
     private final String number;
 
-    private NIF(String number){
+    private NIF(String number) {
         this.number = number;
     }
 
-    public static NIF create(String number){
-        if(!isValid(number)) return new NIF("XXXX");
-        return new NIF(number);
+    public static NIF create(String number) {
+        NIF nif = new NIF(number);
+        if (!nif.isValid()) return new NIF("XXXX");
+        return nif;
     }
 
-    private static boolean isValid(String number){
-        if(number.length() != 9) return false;
-        return number.matches("\\d{8}[A-Z]");
+    private boolean isValid() {
+        if (this.number.length() != 9) return false;
+        return this.number.matches("\\d{8}[A-Z]");
     }
 
     public String getNumber() {
         return this.number;
     }
-
 }
