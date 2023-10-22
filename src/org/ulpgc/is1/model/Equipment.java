7ef1536ac648536f5938437b896043ee152889da
Manuel <manuelcntpublica@yahoo.com>
2023-10-22 13:19:27 +0100

    Updated equipment class, added a constructor for Equipment class, and deleted setter for name.

--- src/org/ulpgc/is1/model/Equipment.java
+++ src/org/ulpgc/is1/model/Equipment.java
@@ -3,12 +3,12 @@ package org.ulpgc.is1.model;
 public class Equipment extends Extra{
     public String name;
 
+    public Equipment(String name) {
+        this.name = name;
+    }
+
     @Override
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
